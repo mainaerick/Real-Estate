c0ca9ab8bc2b5914762b097b5d6f9027a036b781
erick <niteric@gmail.com>
2022-11-08 14:14:36 +0300

    add message controller

--- src/main/java/devric/message/MessageController.java
+++ src/main/java/devric/message/MessageController.java
@@ -3,8 +3,8 @@ package devric.message;
 import devric.appuser.AppUser;
 import devric.appuser.UserRepository;
 import devric.exception_handler.PropertyNotFound;
-import devric.property.Property;
-import devric.property.PropertyRepository;
+import devric.message.Message;
+import devric.message.MessageRepository;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
@@ -17,41 +17,40 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
 import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
 
 public class MessageController {
     @Autowired
-    MessageRepository propertyRepository;
+    MessageRepository messageRepository;
     @Autowired
     UserRepository appUserRepository;
     @GetMapping("/all")
     @PreAuthorize("hasRole('ROLE_ADMIN')")
-    @ApiOperation(value = "${PropertyController.all}", response = Property.class, authorizations = { @Authorization(value="apiKey") })
+    @ApiOperation(value = "${MessageController.all}", response = Message.class, authorizations = { @Authorization(value="apiKey") })
     @ApiResponses(value = {//
             @ApiResponse(code = 400, message = "Something went wrong"), //
             @ApiResponse(code = 403, message = "Access denied"), //
             @ApiResponse(code = 404, message = "doesn't exist"), //
             @ApiResponse(code = 500, message = "Expired or invalid JWT token")})
-    public List<Property> getAll(){
-        return propertyRepository.findAll();
+    public List<Message> getAll(){
+        return messageRepository.findAll();
     }
 
-    @GetMapping("/{propertyId}")
+    @GetMapping("/{messageId}")
     @PreAuthorize("hasRole('ROLE_ADMIN')")
-    @ApiOperation(value = "${PropertyController.property}", response = Property.class, authorizations = { @Authorization(value="apiKey") })
+    @ApiOperation(value = "${MessageController.message}", response = Message.class, authorizations = { @Authorization(value="apiKey") })
     @ApiResponses(value = {//
             @ApiResponse(code = 400, message = "Something went wrong"), //
             @ApiResponse(code = 403, message = "Access denied"), //
-            @ApiResponse(code = 404, message = "Property doesn't exist"), //
+            @ApiResponse(code = 404, message = "Message doesn't exist"), //
             @ApiResponse(code = 500, message = "Expired or invalid JWT token")})
-    public EntityModel<Property> getPropertyById(@PathVariable long propertyId) {
-        Property property = propertyRepository.findById(propertyId).get();
-        if (null == property)
-            throw new PropertyNotFound("property not found");
-        EntityModel model = EntityModel.of(property);
+    public EntityModel<Message> getMessageById(@PathVariable long messageId) {
+        Message message = messageRepository.findById(messageId).get();
+        if (null == message)
+            throw new PropertyNotFound("message not found");
+        EntityModel model = EntityModel.of(message);
         Link link = WebMvcLinkBuilder
                 .linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).getAll()).withRel("all-properies");
         model.add(link);
@@ -60,37 +59,37 @@ public class MessageController {
 
     @PostMapping("/add/{userid}")
     @PreAuthorize("hasRole('ROLE_ADMIN')")
-    @ApiOperation(value = "${PropertyController.addproperty}", response = Property.class, authorizations = { @Authorization(value="apiKey") })
+    @ApiOperation(value = "${MessageController.addmessage}", response = Message.class, authorizations = { @Authorization(value="apiKey") })
     @ApiResponses(value = {//
             @ApiResponse(code = 400, message = "Something went wrong"), //
             @ApiResponse(code = 403, message = "Access denied"), //
             @ApiResponse(code = 404, message = "User doesn't exist"), //
             @ApiResponse(code = 500, message = "Expired or invalid JWT token")})
-    public ResponseEntity<Object> saveProperty(@PathVariable Integer userid, @Valid @RequestBody Property property) {
+    public ResponseEntity<Object> saveMessage(@PathVariable Integer userid, @Valid @RequestBody Message message) {
         AppUser appUser = appUserRepository.findById(userid).get();
         if (null == appUser) {
             throw new PropertyNotFound("User Not Found");
         }
-        property.setUser(appUser);
-        propertyRepository.save(property);
+        message.setAppUser(appUser);
+        messageRepository.save(message);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
                 .path("{departmentId}")
                 .buildAndExpand(appUser.getId())
                 .toUri();
         return ResponseEntity.created(uri).build();
     }
-    @DeleteMapping("/delete/{propertyId}")
+    @DeleteMapping("/delete/{messageId}")
     @PreAuthorize("hasRole('ROLE_ADMIN')")
-    @ApiOperation(value = "${PropertyController.delete}", response = Property.class, authorizations = { @Authorization(value="apiKey") })
+    @ApiOperation(value = "${MessageController.delete}", response = Message.class, authorizations = { @Authorization(value="apiKey") })
     @ApiResponses(value = {//
             @ApiResponse(code = 400, message = "Something went wrong"), //
             @ApiResponse(code = 403, message = "Access denied"), //
-            @ApiResponse(code = 404, message = "Property doesn't exist"), //
+            @ApiResponse(code = 404, message = "Message doesn't exist"), //
             @ApiResponse(code = 500, message = "Expired or invalid JWT token")})
-    public void deleteProperty(@PathVariable Long propertyId) {
-        Property property = propertyRepository.findById(propertyId).get();
-        if (null == property)
-            throw new PropertyNotFound("property not found");
-        propertyRepository.delete(property);
+    public void deleteMessage(@PathVariable Long messageId) {
+        Message message = messageRepository.findById(messageId).get();
+        if (null == message)
+            throw new PropertyNotFound("message not found");
+        messageRepository.delete(message);
     }
 }
