b6afffed00c6c261ec75fc7348455398794557f3
erick <niteric@gmail.com>
2022-11-08 03:13:04 +0300

    add property controller

--- src/main/java/devric/property/PropertyController.java
+++ src/main/java/devric/property/PropertyController.java
@@ -1,6 +1,4 @@
 package devric.property;
-
-
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
@@ -29,7 +27,7 @@ public class PropertyController {
     @Autowired
     UserRepository appUserRepository;
     @GetMapping("/all")
-    @PreAuthorize("hasRole('ROLE_CLIENT')")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @ApiOperation(value = "${PropertyController.all}", response = Property.class, authorizations = { @Authorization(value="apiKey") })
     @ApiResponses(value = {//
             @ApiResponse(code = 400, message = "Something went wrong"), //
@@ -41,6 +39,13 @@ public class PropertyController {
     }
 
     @GetMapping("/{propertyId}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @ApiOperation(value = "${PropertyController.property}", response = Property.class, authorizations = { @Authorization(value="apiKey") })
+    @ApiResponses(value = {//
+            @ApiResponse(code = 400, message = "Something went wrong"), //
+            @ApiResponse(code = 403, message = "Access denied"), //
+            @ApiResponse(code = 404, message = "Property doesn't exist"), //
+            @ApiResponse(code = 500, message = "Expired or invalid JWT token")})
     public EntityModel<Property> getPropertyById(@PathVariable long propertyId) {
         Property property = propertyRepository.findById(propertyId).get();
         if (null == property)
@@ -53,6 +58,13 @@ public class PropertyController {
     }
 
     @PostMapping("/add/{userid}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @ApiOperation(value = "${PropertyController.addproperty}", response = Property.class, authorizations = { @Authorization(value="apiKey") })
+    @ApiResponses(value = {//
+            @ApiResponse(code = 400, message = "Something went wrong"), //
+            @ApiResponse(code = 403, message = "Access denied"), //
+            @ApiResponse(code = 404, message = "User doesn't exist"), //
+            @ApiResponse(code = 500, message = "Expired or invalid JWT token")})
     public ResponseEntity<Object> saveProperty(@PathVariable Integer userid, @Valid @RequestBody Property property) {
         AppUser appUser = appUserRepository.findById(userid).get();
         if (null == appUser) {
@@ -66,13 +78,19 @@ public class PropertyController {
                 .toUri();
         return ResponseEntity.created(uri).build();
     }
-
     @DeleteMapping("/delete/{propertyId}")
+    @GetMapping("/{propertyId}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @ApiOperation(value = "${PropertyController.delete}", response = Property.class, authorizations = { @Authorization(value="apiKey") })
+    @ApiResponses(value = {//
+            @ApiResponse(code = 400, message = "Something went wrong"), //
+            @ApiResponse(code = 403, message = "Access denied"), //
+            @ApiResponse(code = 404, message = "Property doesn't exist"), //
+            @ApiResponse(code = 500, message = "Expired or invalid JWT token")})
     public void deleteProperty(@PathVariable Long propertyId) {
         Property property = propertyRepository.findById(propertyId).get();
         if (null == property)
             throw new PropertyNotFound("property not found");
         propertyRepository.delete(property);
-
     }
 }
